e7d487e5dc909b89051342b13e9e621fde84996d
ashikurashik <ar3273167@gmail.com>
2021-07-16 22:06:46 +0600

    update for css file

--- js/main.js
+++ js/main.js
@@ -10,5 +10,17 @@ $(".toggle").on('click', function(){
 
 /*Product Slider 4 Column*/
 
+$('.product-carousel').slick({
+  dots: false,
+  infinite: true,
+  speed: 800,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: '<button type="button" class="slick-prev"><i class="fal fa-angle-double-left"></i></button>',
+  nextArrow: '<button type="button" class="slick-next"><i class="fal fa-angle-double-right"></i></button>',
+});
+
+
+
 
 })(jQuery);
