b3b7c7f4a5f725c7925582e742c18b2af62e9855
ashikurashik <ar3273167@gmail.com>
2021-07-18 23:00:54 +0600

    update for js file

--- js/main.js
+++ js/main.js
@@ -20,15 +20,40 @@ $('.product-carousel').slick({
   nextArrow: '<button type="button" class="slick-next"><i class="fal fa-angle-double-right"></i></button>',
 });
 
-$('.product-carousel.deal-slider').slick({
-  dots: false,
-  infinite: true,
-  speed: 800,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  prevArrow: '<button type="button" class="slick-prev"><i class="fal fa-angle-double-left"></i></button>',
-  nextArrow: '<button type="button" class="slick-next"><i class="fal fa-angle-double-right"></i></button>',
-});
+
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  dots: true,
+  margin: 15,
+  slideSpeed: 1000,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  resonsiveClass: true,
+  responsiveRefreshRate: true,
+  responsive: {
+      0 : {
+          items: 1
+      },
+      768 : {
+          items: 2
+      },
+      1000 : {
+          items: 2
+      },
+      1200 : {
+          items: 2
+      },
+      1920 : {
+          items: 3
+      }
+  }
+});  
+
+wow = new WOW();
+wow.init();
+
 
 
 
